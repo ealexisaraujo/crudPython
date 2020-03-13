d07a572c0348bdff6e3f2e8aa3487eee9f0bd6e0
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-13 18:04:05 -0500

    6th: Operaciones matemáticas

---
+++ operadoresMatematicos.py
@@ -0,0 +1,64 @@
+print('>>> 1 + 2')
+print( 1+2)
+
+print('>>> 2 - 5')
+print( 2-5)
+
+print('>>> 10 / 4')
+print( 10/4)
+
+print('>>> 10 // 4')
+print( 10//4)
+
+print('>>> 10 % 4')
+print( 10%4)
+
+print('>>> 20 % 5')
+print( 20 % 5)
+
+print('>>> 21 % 5')
+print( 21 % 5)
+
+print('>>> 22 % 5')
+print( 22 % 5)
+
+print('>>> 10 * 2')
+print( 10*2)
+
+print('>>> 2 ** 8')
+print( 2 ** 8 )
+
+print('>>> type(5)')
+print(type(5))
+
+print('>>> type(5.5)')
+print(type(5.5))
+
+print('>>> type(\'David\')')
+print(type('David'))
+
+print('>>> \'a\' + \'b\'')
+print('a' + 'b' )
+
+print('>>> \'a\' / 5')
+print('************************** ERROR **************************')
+print('Traceback (most recent call last):')
+print('\tFile "<stdin>", line 1, in <module>')
+print('\t\tprint(\'a\'/5)')
+print('TypeError: unsupported operand type(s) for /: \'str\' and \'int\'')
+print('************************** ERROR **************************\n')
+
+print('>>> \'b\' * 5')
+print('b' * 5)
+
+print('>>> \'Platzi\' * 5')
+print('Platzi' * 5)
+
+print('>>> a = 5')
+a = 5
+
+print('>>> a += 3')
+a += 3
+
+print('>>> print(a)')
+print(a)
