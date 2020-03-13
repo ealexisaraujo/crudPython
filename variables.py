610990395efdb7ad2d795b83a4222f92a51d92f3
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-13 18:08:35 -0500

    7th: Variables y expresiones

---
+++ variables.py
@@ -0,0 +1,35 @@
+print('>>> message = \'Hola\'')
+message = 'Hola'
+
+print('>>> print(message)')
+print(message)
+
+print('>>> _other_message = \'Hey\'')
+_other_message = 'Hey'
+
+print('>>> print(_other_message)')
+print(_other_message)
+
+print('>>> 23var = 5')
+print('  File "<stdin>", line 1')
+print('    23var = 5')
+print('        ^')
+print('SyntaxError: invalid syntax\n\n')
+
+print('>>> global = 23')
+print('  File "<stdin>", line 1')
+print('    23var = 5')
+print('        ^')
+print('SyntaxError: invalid syntax\n\n')
+
+print('>>> global_sales = 23')
+global_sales = 23
+
+print('>>> 2 + 7 / 4')
+print( 2 + 7 / 4)
+
+print('>>> 7 / 4')
+print( 7 / 4)
+
+print('>>> 9 / 4')
+print( 9 / 4)
