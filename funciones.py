4c83d4101136fef7aca7950fa78f4df6e462355a
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-14 09:53:37 -0500

    9th: Funciones

---
+++ funciones.py
@@ -0,0 +1,51 @@
+print('>>> type(1)')
+print(type(1))
+
+print('>>> un_entero = int(\'5\')')
+un_entero = int('5')
+
+
+print('>>> print(un_entero)')
+print(un_entero)
+
+print('>>> type(un_entero)')
+print(type(un_entero))
+
+print('>>> a = bool(\'a\')')
+a = bool('a')
+
+print('>>> print(a)')
+print(a)
+
+print('>>> a = bool(\'\')')
+a = bool('')
+
+print('>>> print(a)')
+print(a)
+
+print('>>> a = float(3)')
+a = float(3)
+
+print('>>> type(a)')
+print(type(a))
+
+print('>>> def suma_de_dos_numeros(x,y):')
+print('...     return x + y')
+print('...')
+def suma_de_dos_numeros(x,y):
+    return x + y
+
+print('>>> suma_de_dos_numeros(10, 15)')
+print(suma_de_dos_numeros(10, 15))
+
+print('>>> type(suma_de_dos_numeros)')
+print(type(suma_de_dos_numeros))
+
+print('>>> suma_total = suma_de_dos_numeros(10, 15)')
+suma_total = suma_de_dos_numeros(10, 15)
+
+print('>>> print(suma_total)')
+print(suma_total)
+
+print('>>> type(suma_total)')
+print(type(suma_total))
