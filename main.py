a1d3723ee96fdec18be16580ba8b797fe4baa283
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-14 11:30:07 -0500

    10th:Usando Funciones en nuestro proyecto

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-clients = 'Pablo, Ricardo,'
+clients = 'tomas,juan,'
 
-if __name__=='__main__':
-    clients += ' David'
+
+def create_client(client_name): #1
+    global clients
+
+    clients += client_name
+    _add_comma()
+
+
+def list_clients(): #3
+    global clients
+
     print(clients)
+
+
+def _add_comma(): #2 
+    global clients
+
+    clients += ','
+
+
+if __name__ == '__main__':
+    client_name = 'Cristian'
+
+    list_clients()
+    create_client(client_name)
+
+    list_clients()
