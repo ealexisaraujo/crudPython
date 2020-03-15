34195802be3bf93292d33e7b39bd746651418933
Alexis Araujo <ala1289@gmail.com>
2020-03-15 10:53:47 -0600

    Operaciones con strings y el comando Update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 clients = 'pablo,ricardo,'
 
 
@@ -15,6 +17,15 @@ def list_client():
     print(clients)
 
 
+def update_client(client_name, updated_name):
+    global clients
+
+    if client_name in clients:
+        clients = clients.replace(client_name + ',', updated_name + ',')
+    else:
+        print('Client not in client\'s list')
+
+
 def _add_comma():
     global clients
     clients += ','
@@ -24,18 +35,41 @@ def _print_welcome():
     print('Welcome to platzi ventas')
     print('*'*50)
     print('[C]reat client')
+    print('[U]pdate client')
     print('[D]elete client')
+
+
+def _get_client_name():
+    client_name = None
+
+    while not client_name:
+        client_name = input('What is the client name?')
+
+        if client_name == 'exit':
+            client_name = None
+            break
+
+    if not client_name:
+        sys.exit()
+
+    return client_name
 
 
 if __name__ == '__main__':
     _print_welcome()
     command = input()
+    command = command.upper()
 
     if command == 'C':
-        client_name = input('What is the client name? ')
+        client_name = _get_client_name()
         create_client(client_name)
         list_client()
     elif command == 'D':
         pass
+    elif command == 'U':
+        client_name = _get_client_name()
+        updated_name = input('What is the new client name? ')
+        update_client(client_name, updated_name)
+        list_client()
     else:
         print('Invalid command')
