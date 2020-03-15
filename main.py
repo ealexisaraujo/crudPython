1a43c41f307848b0468c10e7e26cd085a67be616
Alexis Araujo <ala1289@gmail.com>
2020-03-15 09:22:07 -0600

    Estructuras Condicionales

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ clients = 'pablo,ricardo,'
 
 def create_client(client_name):
     global clients
-    clients += client_name
-    _add_comma()
+    if client_name not in clients:
+        clients += client_name
+        _add_comma()
+    else:
+        print('Client already is in the client\'s list')
 
 
 def list_client():
@@ -17,7 +20,22 @@ def _add_comma():
     clients += ','
 
 
+def _print_welcome():
+    print('Welcome to platzi ventas')
+    print('*'*50)
+    print('[C]reat client')
+    print('[D]elete client')
+
+
 if __name__ == '__main__':
-    list_client()
-    create_client('David')
-    list_client()
+    _print_welcome()
+    command = input()
+
+    if command == 'C':
+        client_name = input('What is the client name? ')
+        create_client(client_name)
+        list_client()
+    elif command == 'D':
+        pass
+    else:
+        print('Invalid command')
