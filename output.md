b2cfe0e7c3d30560655039569214236adec56f62
Cristian F. Tovar <cristhiam.tovar@mail.escuelaing.edu.co>
2020-03-16 19:27:58 -0500

    27th:Tuplas y conjuntos de código

--- output.md
+++ output.md
@@ -1,22 +1,80 @@
->>> mi_tupla = 1,2,3
->>> tup = (1,2,3)
->>> tup = mi_tupla
-True
+>>> a = 1,2,3
+>>> type(a)
+<class 'tuple'>
+>>> a[0]
+1
+>>> a[1]
+2
+>>> a[2]
+3
+>>> a[3]
+************************** ERROR **************************
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+IndexError: tuple index out of range
+************************** ERROR **************************
+
+
+>>> a[0]= 10
+************************** ERROR **************************
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'tuple' object does not support item assignment
+************************** ERROR **************************
+
+
+>>> a = 'Cristian'
+>>> a[0]
+C
+>>> a[0]= d
+************************** ERROR **************************
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'str' object does not support item assignment
+************************** ERROR **************************
+
+
+>>> a = (1,1,1,2,3,4)
+>>> type(a)
+<class 'tuple'>
+>>> dir(a)
+['__add__', '__class__', '__contains__', '__delattr__', '__dir__', '__doc__', '__eq__', '__format__', '__ge__', '__getattribute__', '__getitem__', '__getnewargs__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__len__', '__lt__', '__mul__', '__ne__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__rmul__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', 'count', 'index']
+>>> a.count(1)
+3
+>>> a.count(2)
+1
+>>> a.index(3)
+4
+>>>
+>>>
+>>>
 >>> a = set([1,2,3])
->>> b = set([3, 4, 5])
->>> a.union(b
-{1, 2, 3, 4, 5}
+>>> b = {3, 4, 5}
+>>> type(a)
+<class 'set'>
+>>> type(b)
+<class 'set'>
+>>> a[1]
+************************** ERROR **************************
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+TypeError: 'set' object is not subscriptable
+************************** ERROR **************************
+
+
+>>> a.add(3)
+None
+>>> a
+{1, 2, 3}
+>>> a.add(20)
+None
+>>> a
+{1, 2, 3, 20}
 >>> a.intersection(b)
 {3}
+>>> a.union(b)
+{1, 2, 3, 20, 4, 5}
 >>> a.difference(b)
-{1, 2}
->>> b.difference(b)
-set()
->>> 1 in a
-True
->>> 1 in b
-False
->>> 1 not in a
-False
->>> 1 not in b
-True
+{1, 2, 20}
+>>> b.difference(a)
+{4, 5}
--- tuplas&conjuntos.py
+++ tuplas&conjuntos.py
@@ -1,38 +1,104 @@
-print('>>> mi_tupla = 1,2,3')
-mi_tupla = 1,2,3
+print('>>> a = 1,2,3')
+a = 1,2,3
 
-print('>>> tup = (1,2,3)')
-tup = (1,2,3)
+print('>>> type(a)')
+print( type(a))
 
-print('>>> tup = mi_tupla')
-print(tup == mi_tupla)
+print('>>> a[0]')
+print(a[0])
 
+print('>>> a[1]')
+print(a[1])
+
+print('>>> a[2]')
+print(a[2])
+
+print('>>> a[3]')
+print('************************** ERROR **************************')
+print('Traceback (most recent call last):')
+print('  File "<stdin>", line 1, in <module>')
+print('IndexError: tuple index out of range')
+print('************************** ERROR **************************\n\n')
+
+
+print('>>> a[0]= 10')
+print('************************** ERROR **************************')
+print('Traceback (most recent call last):')
+print('  File "<stdin>", line 1, in <module>')
+print('TypeError: \'tuple\' object does not support item assignment')
+print('************************** ERROR **************************\n\n')
+
+print('>>> a = \'Cristian\'')
+a = 'Cristian'
+
+print('>>> a[0]')
+print(a[0])
+
+print('>>> a[0]= d')
+print('************************** ERROR **************************')
+print('Traceback (most recent call last):')
+print('  File "<stdin>", line 1, in <module>')
+print('TypeError: \'str\' object does not support item assignment')
+print('************************** ERROR **************************\n\n')
+
+print('>>> a = (1,1,1,2,3,4)')
+a = (1,1,1,2,3,4)
+
+print('>>> type(a)')
+print( type(a))
+
+print('>>> dir(a)')
+print(dir(a))
+
+print('>>> a.count(1)')
+print(a.count(1))
+
+print('>>> a.count(2)')
+print(a.count(2))
+
+print('>>> a.index(3)')
+print(a.index(3))
+
+print('>>>\n>>>\n>>>')
 print('>>> a = set([1,2,3])')
 a = set([1,2,3])
 
-print('>>> b = set([3, 4, 5])')
-b = set([3, 4, 5])
+print('>>> b = {3, 4, 5}')
+b = {3, 4, 5}
 
-print('>>> a.union(b')
-print(a.union(b))
+print('>>> type(a)')
+print( type(a))
+
+print('>>> type(b)')
+print( type(b))
+
+print('>>> a[1]')
+print('************************** ERROR **************************')
+print('Traceback (most recent call last):')
+print('  File "<stdin>", line 1, in <module>')
+print('TypeError: \'set\' object is not subscriptable')
+print('************************** ERROR **************************\n\n')
+
+print('>>> a.add(3)')
+print(a.add(3))
+
+print('>>> a')
+print(a)
+
+print('>>> a.add(20)')
+print(a.add(20))
+
+print('>>> a')
+print(a)
 
 print('>>> a.intersection(b)')
 print(a.intersection(b))
 
+print('>>> a.union(b)')
+print(a.union(b))
+
 print('>>> a.difference(b)')
 print(a.difference(b))
 
-print('>>> b.difference(b)')
-print(b.difference(b))
-
-print('>>> 1 in a')
-print(1 in a)
-
-print('>>> 1 in b')
-print(1 in b)
-
-print('>>> 1 not in a')
-print(1 not in a)
-
-print('>>> 1 not in b')
-print(1 not in b)
+print('>>> b.difference(a)')
+print(b.difference(a))
